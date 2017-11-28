3ca44399fc31243743a185b3147fd96495e03b25
kenboo98 <kenboo1998@gmail.com>
2017-11-27 22:12:20 -0700

    for fork

--- src/playscreen.h
+++ src/playscreen.h
@@ -5,12 +5,29 @@ class PlayScreen{
 
 private:
 	Adafruit_ILI9341* tft;
-
-public:
- 	PlayScreen(Adafruit_ILI9341* tft);
-	void drawPlay(int state);
+	//state of the play button
+	bool isPlaying,isLooping;
+	uint8_t volume;
+	uint32_t index;
+	//time since the volume has changed
+	int delay;
+	void onPlayClick();
+	void onForwardClick();
+	void onReverseClick();
+	void onLoopClick();
+	void onVolUpClick();
+	void onVolDownClick();
+	void onAlbumClick();
+	void onProgressBarClick(int percentage);
+	void drawVolumeBar();
 	void drawProgressBar(int progress);
 	void drawAlbum();
 	void drawInfo(const char* title,int len);
+	bool isTouched(int tx, int ty, int x, int y, int w, int h);
+	void draw();
+public:
+ 	PlayScreen(Adafruit_ILI9341* tft);
+	void handleTouch(Touch* touch);
+	
 };
 #endif
